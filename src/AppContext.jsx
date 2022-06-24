66c72e1151ba4f67a83f6f496d21aa753092c64f
Dawid <dawidmuraszewski@web.de>
2022-06-24 15:52:28 +0200

    dawid added nouns to context

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,12 +1,25 @@
 import { createContext } from 'react';
-
+import { useState, useEffect } from "react";
+import axios from 'axios';
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+	const [nouns, setNouns] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const nounsUrl = "https://edwardtanguay.netlify.app/share/germanNouns.json";
+			const _nouns = (await axios.get(nounsUrl)).data;
+			setNouns(_nouns);
+		})();
+	}, []);
+
 	return (
 		<AppContext.Provider
 			value={{
 				siteTitle: 'Web Team IT-Rex',
+				nouns,
+				setNouns
 			}}
 		>
 			{children}
--- src/pages/members/PageMembersDawid.jsx
+++ src/pages/members/PageMembersDawid.jsx
@@ -2,11 +2,17 @@ import { useContext } from 'react';
 import { AppContext } from '../../AppContext';
 
 export const PageMembersDawid = () => {
-	const { siteTitle } = useContext(AppContext);
-
+	const { siteTitle, nouns, setNouns } = useContext(AppContext);
+	const handleDeleteNoun = () => {
+		nouns.pop();
+		setNouns([...nouns]);
+	}
 	return (
+
 		<>
 			<p>This is a "{siteTitle}":</p>
+			<p>There {nouns.length} nouns.</p>
+			<button onClick={handleDeleteNoun}>delete noun</button>
 			<ul>
 				<li>cheerio.js</li>
 				<li>chart.js</li>
